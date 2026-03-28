4c024b95cdfa269403ab696b06efc63793432d1e
Shoan Genes <shoangenes42@gmail.com>
2026-03-27 23:34:55 -0300

    chore(project): add dev profile configuration

---
+++ src/main/java/dev/genesshoan/fitness_management_api/FitnessManagementApiApplication.java
@@ -0,0 +1,13 @@
+package dev.genesshoan.fitness_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessManagementApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FitnessManagementApiApplication.class, args);
+  }
+
+}
