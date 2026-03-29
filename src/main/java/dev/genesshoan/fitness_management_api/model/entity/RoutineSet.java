547fbb5a872377f5c36c15ae42930496b40592b2
Shoan Genes <shoangenes42@gmail.com>
2026-03-28 22:02:50 -0300

    refactor(entities): replace composite keys with synthetic id in routine_set and training_set
    - Delete TrainingSetId and RoutineSetId composite keys
    - Implement synthetic ids in routine_set and training_set
    - Implement unique constraint with exercise_id, routine_id, number and
      block columns

--- src/main/java/dev/genesshoan/fitness_management_api/model/entity/RoutineSet.java
+++ src/main/java/dev/genesshoan/fitness_management_api/model/entity/RoutineSet.java
@@ -1,43 +1,54 @@
 package dev.genesshoan.fitness_management_api.model.entity;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.MapsId;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "routine_sets")
+@Table(name = "routine_sets", uniqueConstraints = @UniqueConstraint(name = "uk_routine_exercise_block_number", columnNames = {
+    "routine_id", "exercise_id", "block", "number" }))
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 public class RoutineSet {
-  @EmbeddedId
-  private RoutineSetId routineSetId;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
   @ManyToOne(fetch = FetchType.LAZY)
-  @MapsId("routineId")
+  @JoinColumn(name = "routine_id", nullable = false)
   private Routine routine;
 
   @ManyToOne(fetch = FetchType.LAZY)
-  @MapsId("exerciseId")
+  @JoinColumn(name = "exercise_id", nullable = false)
   private Exercise exercise;
 
   @Column(nullable = false)
-  private double targetWeight;
+  private Integer block;
 
   @Column(nullable = false)
-  private int targetReps;
+  private Integer number;
 
   @Column(nullable = false)
-  private int restMinutes;
+  private Double targetWeight;
+
+  @Column(nullable = false)
+  private Integer targetReps;
+
+  @Column(nullable = false)
+  private Integer restSeconds;
 
   @Override
   public int hashCode() {
@@ -53,10 +64,10 @@ public class RoutineSet {
     if (getClass() != obj.getClass())
       return false;
     RoutineSet other = (RoutineSet) obj;
-    if (routineSetId == null) {
-      if (other.routineSetId != null)
+    if (id == null) {
+      if (other.id != null)
         return false;
-    } else if (!routineSetId.equals(other.routineSetId))
+    } else if (!id.equals(other.id))
       return false;
     return true;
   }
--- src/main/java/dev/genesshoan/fitness_management_api/model/entity/TrainingSet.java
+++ src/main/java/dev/genesshoan/fitness_management_api/model/entity/TrainingSet.java
@@ -1,40 +1,51 @@
 package dev.genesshoan.fitness_management_api.model.entity;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.MapsId;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "training_sets")
+@Table(name = "training_sets", uniqueConstraints = @UniqueConstraint(name = "uk_training_exercise_block_number", columnNames = {
+    "training_id", "exercise_id", "block", "number" }))
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 public class TrainingSet {
-  @EmbeddedId
-  private TrainingSetId trainingSetId;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
   @ManyToOne(fetch = FetchType.LAZY)
-  @MapsId("trainingId")
+  @JoinColumn(name = "training_id", nullable = false)
   private Training training;
 
   @ManyToOne(fetch = FetchType.LAZY)
-  @MapsId("exerciseId")
+  @JoinColumn(name = "exercise_id", nullable = false)
   private Exercise exercise;
 
   @Column(nullable = false)
-  private double weight;
+  private Integer block;
 
   @Column(nullable = false)
-  private int reps;
+  private Integer number;
+
+  @Column(nullable = false)
+  private Double weight;
+
+  @Column(nullable = false)
+  private Integer reps;
 
   @Override
   public int hashCode() {
@@ -50,10 +61,10 @@ public class TrainingSet {
     if (getClass() != obj.getClass())
       return false;
     TrainingSet other = (TrainingSet) obj;
-    if (trainingSetId == null) {
-      if (other.trainingSetId != null)
+    if (id == null) {
+      if (other.id != null)
         return false;
-    } else if (!trainingSetId.equals(other.trainingSetId))
+    } else if (!id.equals(other.id))
       return false;
     return true;
   }
